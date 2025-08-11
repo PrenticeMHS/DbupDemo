141a27418514cec7199db2e0af5d9294fca0befe
Prentice Lauzon <prentice.lauzon@mhs.com>
2025-08-11 11:36:01 -0400

    Added a new another user in the seeding file

---
+++ DbupDemo/Scripts/Seeding/202406111134-AddAnotherNewUser.sql
@@ -0,0 +1,5 @@
+-- Adds a new user to the database
+
+INSERT INTO [dbo].[User] ([Id], [Name], [Email], [CreatedDate], [DeleteDate])
+VALUES
+    (NEWID(), 'Test User 4', 'testuser4@example.com', GETDATE(), NULL)
